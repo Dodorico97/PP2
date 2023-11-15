69506caf862ca4a1372857666ec197696f99a89f
Joaquin Dodorico <144488169+Dodorico97@users.noreply.github.com>
2023-11-15 09:53:53 -0300

    Update carrito.js

--- carrito.js
+++ carrito.js
@@ -1,1 +1,119 @@
+let carrito = [];
 
+const dialog = document.getElementById('customDialog');
+const confirmButton = document.getElementById('confirmVaciar');
+const cancelButton = document.getElementById('cancelVaciar');
+
+confirmButton.addEventListener('click', function() {
+    carrito.length = 0;
+    localStorage.removeItem('carrito');
+    mostrarCarrito();
+    hideCustomDialog();
+});
+
+cancelButton.addEventListener('click', hideCustomDialog);
+
+function showCustomDialog() {
+    dialog.style.display = 'block';
+}
+
+function hideCustomDialog() {
+    dialog.style.display = 'none';
+}
+
+confirmButton.addEventListener('click', function() {
+    vaciarCarrito();
+    hideCustomDialog();
+});
+
+cancelButton.addEventListener('click', hideCustomDialog);
+
+function mostrarCarrito() {
+    const listaCarrito = document.getElementById("lista-carrito");
+    listaCarrito.innerHTML = '';
+
+    let total = 0;
+
+    carrito.forEach(producto => {
+        total += producto.precio * producto.cantidad;
+
+        const li = document.createElement("li");
+        li.classList.add('item-carrito');
+        
+        const spanEliminar = document.createElement("span");
+        spanEliminar.classList.add('producto-eliminar');
+        const imagenEliminar = document.createElement("img");
+        imagenEliminar.setAttribute('src', 'Imagenes/imageneliminar.png');
+        imagenEliminar.setAttribute('alt', 'Eliminar');
+        imagenEliminar.classList.add('imagen-eliminar');
+        imagenEliminar.onclick = function() {
+            eliminarDelCarrito(producto.id);
+        }
+        spanEliminar.appendChild(imagenEliminar);
+        
+        const spanNombre = document.createElement("span");
+        spanNombre.classList.add('producto-nombre');
+        spanNombre.textContent = producto.nombre;
+
+        const spanPrecio = document.createElement("span");
+        spanPrecio.classList.add('producto-precio');
+        spanPrecio.textContent = '$' + producto.precio;
+
+        const spanCantidad = document.createElement("span");
+        spanCantidad.classList.add('producto-cantidad');
+        spanCantidad.textContent = producto.cantidad;
+
+        li.appendChild(spanEliminar);
+        li.appendChild(spanNombre);
+        li.appendChild(spanPrecio);
+
+        listaCarrito.appendChild(li);
+    });
+
+    actualizarTotal();
+}
+
+function eliminarDelCarrito(idProductoAEliminar) {
+    carrito = carrito.filter(producto => producto.id !== idProductoAEliminar);
+    guardarCarritoEnLocalStorage();
+    mostrarCarrito();
+}
+
+function cerrarCarrito() {
+    document.getElementById("carrito").style.display = "none";
+}
+
+function guardarCarritoEnLocalStorage() {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+function cargarCarritoDesdeLocalStorage() {
+    const carritoGuardado = localStorage.getItem('carrito');
+    if (carritoGuardado) {
+        carrito = JSON.parse(carritoGuardado);
+    }
+}
+
+function actualizarTotal() {
+    let total = carrito.reduce((acc, producto) => acc + producto.precio * producto.cantidad, 0);
+    document.getElementById("precio-total").textContent = "$" + total.toFixed(2);
+}
+
+function vaciarCarrito() {
+    showCustomDialog();
+}
+
+function redirigirAWhatsApp() {
+    let numeroWhatsApp = "2657584675";
+    
+    let mensaje = "¡Hola! Estoy interesado en hacer una compra. Estos son los productos que quiero:\n\n";
+    carrito.forEach(producto => {
+        mensaje += `* ${producto.nombre} - Precio: $${producto.precio}\n`;
+    });
+    mensaje += "\n¿Podrías ayudarme con eso?";
+    
+    let url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensaje)}`;
+    window.open(url, '_blank');
+}
+
+cargarCarritoDesdeLocalStorage();
